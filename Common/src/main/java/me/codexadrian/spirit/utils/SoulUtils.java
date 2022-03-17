9375c6afc0c50bacb040762b658f4188fcb543d4
abused_master <techperson71@gmail.com>
2022-03-16 23:04:49 -0400

    Update SoulUtils.java

--- Common/src/main/java/me/codexadrian/spirit/utils/SoulUtils.java
+++ Common/src/main/java/me/codexadrian/spirit/utils/SoulUtils.java
@@ -1,7 +1,6 @@
 package me.codexadrian.spirit.utils;
 
 import me.codexadrian.spirit.Spirit;
-import me.codexadrian.spirit.SpiritRegistry;
 import me.codexadrian.spirit.Tier;
 import net.minecraft.world.item.ItemStack;
 
@@ -10,90 +9,103 @@ import java.util.Arrays;
 public class SoulUtils {
 
     public static Tier getTier(ItemStack itemStack) {
-        if(!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
+        if (!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
             return null;
         }
+
         int storedSouls = itemStack.getTag().getCompound("StoredEntity").getInt("Souls");
         String type = itemStack.getTag().getCompound("StoredEntity").getString("Type");
         Tier tier = null;
-        for(Tier t : Spirit.getSpiritConfig().getTiers()) {
-            if(Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
+
+        for (Tier t : Spirit.getSpiritConfig().getTiers()) {
+            if (Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
                 if (t.getRequiredSouls() <= storedSouls) {
                     tier = t;
-                } else {
-                    break;
+                    continue;
                 }
+                
+                break;
             }
         }
+
         return tier;
     }
 
     public static int getTierIndex(ItemStack itemStack) {
-        if(!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
+        if (!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
             return -1;
         }
+
         int storedSouls = itemStack.getTag().getCompound("StoredEntity").getInt("Souls");
         String type = itemStack.getTag().getCompound("StoredEntity").getString("Type");
         int tier = 0;
-        for(int i = 0; i < Spirit.getSpiritConfig().getTiers().length; i++) {
+        for (int i = 0; i < Spirit.getSpiritConfig().getTiers().length; i++) {
             Tier t = Spirit.getSpiritConfig().getTiers()[i];
-            if(Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
+            if (Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
                 if (t.getRequiredSouls() <= storedSouls) {
                     tier = i;
-                } else {
-                    break;
+                    continue;
                 }
+
+                break;
             }
         }
+
         return tier;
     }
 
     public static Tier getNextTier(ItemStack itemStack) {
-        if(!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
+        if (!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
             return null;
         }
+
         int storedSouls = itemStack.getTag().getCompound("StoredEntity").getInt("Souls");
         String type = itemStack.getTag().getCompound("StoredEntity").getString("Type");
         Tier tier = null;
-        for(Tier t : Spirit.getSpiritConfig().getTiers()) {
-            if(Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
+        for (Tier t : Spirit.getSpiritConfig().getTiers()) {
+            if (Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
                 if (t.getRequiredSouls() > storedSouls) {
                     tier = t;
                     break;
                 }
             }
         }
+
         return tier;
     }
 
     public static int getMaxSouls(ItemStack itemStack) {
-        if(!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
+        if (!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
             return Integer.MAX_VALUE;
         }
+
         String type = itemStack.getTag().getCompound("StoredEntity").getString("Type");
         int requiredSouls = 0;
-        for(int i = 0; i < Spirit.getSpiritConfig().getTiers().length; i++) {
+        for (int i = 0; i < Spirit.getSpiritConfig().getTiers().length; i++) {
             Tier t = Spirit.getSpiritConfig().getTiers()[i];
-            if(Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
-                if(requiredSouls < t.getRequiredSouls()) {
+            if (Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
+                if (requiredSouls < t.getRequiredSouls()) {
                     requiredSouls = t.getRequiredSouls();
                 }
             }
         }
+
         return requiredSouls;
     }
 
     public static Tier getMaxTier(ItemStack itemStack) {
-        if(!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
+        if (!itemStack.hasTag() || !itemStack.getTag().contains("StoredEntity")) {
             return null;
         }
+
         String type = itemStack.getTag().getCompound("StoredEntity").getString("Type");
         Tier tier = null;
-        for(Tier t : Spirit.getSpiritConfig().getTiers()) {
-            if(Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
+        for (Tier t : Spirit.getSpiritConfig().getTiers()) {
+            if (Arrays.stream(t.getBlacklist()).noneMatch(b -> b.equals(type))) {
                 tier = t;
             }
         }
+
         return tier;
     }
 
