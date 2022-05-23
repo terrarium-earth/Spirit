7fa98a05c4b89a3dda800a38364269fe61ae908f
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-05-23 18:18:29 -0400

    Entities now show if they have a soul when hovered over

--- Forge/src/main/java/me/codexadrian/spirit/compat/TOPCompat.java
+++ Forge/src/main/java/me/codexadrian/spirit/compat/TOPCompat.java
@@ -2,11 +2,15 @@ package me.codexadrian.spirit.compat;
 
 import mcjty.theoneprobe.api.*;
 import me.codexadrian.spirit.Constants;
+import me.codexadrian.spirit.Corrupted;
 import me.codexadrian.spirit.Spirit;
 import me.codexadrian.spirit.SpiritRegistry;
 import me.codexadrian.spirit.blocks.soulcage.SoulCageBlockEntity;
 import me.codexadrian.spirit.utils.SoulUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -28,8 +32,22 @@ public class TOPCompat implements Function<ITheOneProbe, Void> {
                     if(blockEntity.isEmpty()) {
                         probeInfo.horizontal().text("block.spirit.soul_cage.empty_hover_text");
                     } else if(blockEntity.type != null) {
-                        probeInfo.horizontal().text("").text(blockEntity.type.getDescription()).text(" - Tier " + SoulUtils.getTierIndex(blockEntity.getItem(0)));
+                        probeInfo.horizontal().entity(blockEntity.type.create(level)).vertical().text(" ").horizontal().text(blockEntity.type.getDescription()).text(" - Tier " + (SoulUtils.getTierIndex(blockEntity.getItem(0)) + 1) + " ");
                     }
+                }
+            }
+        });
+        theOneProbe.registerEntityProvider(new IProbeInfoEntityProvider() {
+            @Override
+            public String getID() {
+                return Constants.MODID + ":corrupted_info";
+            }
+
+            @Override
+            public void addProbeEntityInfo(ProbeMode probeMode, IProbeInfo probeInfo, Player player, Level level, Entity entity, IProbeHitEntityData iProbeHitEntityData) {
+                Corrupted corrupted = (Corrupted) entity;
+                if(corrupted.isCorrupted()) {
+                    probeInfo.text("effect.spirit.corrupted");
                 }
             }
         });
