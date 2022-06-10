af0abce9a5d671471c1286a00a21a56f7913a4c7
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-06-10 16:25:50 -0400

    AAA

--- common/src/main/java/me/codexadrian/spirit/utils/SoulUtils.java
+++ common/src/main/java/me/codexadrian/spirit/utils/SoulUtils.java
@@ -15,7 +15,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
-import javax.annotation.Nullable;
 import java.util.Arrays;
 
 public class SoulUtils {
