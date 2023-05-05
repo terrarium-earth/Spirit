c14173bc2d304250a3a3a964ecc653113e932e9e
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2023-05-05 15:02:31 -0400

    Fix

--- common/src/main/java/me/codexadrian/spirit/blocks/blockentity/PedestalBlockEntity.java
+++ common/src/main/java/me/codexadrian/spirit/blocks/blockentity/PedestalBlockEntity.java
@@ -87,8 +87,8 @@ public class PedestalBlockEntity extends BlockEntity implements WorldlyContainer
     @Override
     public void load(@NotNull CompoundTag compoundTag) {
         super.load(compoundTag);
-        if (compoundTag.contains("Item")) {
-            item = ItemStack.of(compoundTag.getCompound("Item"));
+        if (compoundTag.contains("item")) {
+            item = ItemStack.of(compoundTag.getCompound("item"));
         } else {
             item = ItemStack.EMPTY;
         }
@@ -98,9 +98,9 @@ public class PedestalBlockEntity extends BlockEntity implements WorldlyContainer
     protected void saveAdditional(@NotNull CompoundTag compoundTag) {
         super.saveAdditional(compoundTag);
         if (!item.isEmpty()) {
-            compoundTag.put("Item", item.save(new CompoundTag()));
+            compoundTag.put("item", item.save(new CompoundTag()));
         } else {
-            compoundTag.remove("Item");
+            compoundTag.remove("item");
         }
     }
 
